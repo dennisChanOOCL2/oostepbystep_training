bba8ac5eb4c97266648e332e8cbfc0f283eded61
Dennis Chan <dennis.chan@oocl.com>
2020-04-07 15:01:47 +0800

    add cls attribute to Student

--- oo-stepbystep/src/main/java/com/oocl/Student.java
+++ oo-stepbystep/src/main/java/com/oocl/Student.java
@@ -1,8 +1,24 @@
 package com.oocl;
 
 public class Student extends Person {
+
+    private Class cls;
+
+    @Override
+    public Class getCls() {
+        return cls;
+    }
+
+    @Override
+    public void setCls(Class cls) {
+        this.cls = cls;
+    }
+
     @Override
     public String introduce(){
-        return String.format("There is a student who will introduce himself like this: My name is %s. I am %s years old. Coding for the glory of OOCL.", this.getName(), this.getAge());
+        return String.format("There is a student who will introduce himself like this: " +
+                "My name is %s. I am %s years old. " +
+                "I am a student of class %s." +
+                "Coding for the glory of OOCL.", this.getName(), this.getCls(), this.getAge());
     };
 }
